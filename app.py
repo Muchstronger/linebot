8c9f922874b4b63d102b68d9931d3d9d92e06e42
cccodaa <95553367+Muchstronger@users.noreply.github.com>
2025-05-27 14:18:06 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,109 @@
+from flask import Flask, request, abort
+
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage,
+    TemplateMessage,
+    ButtonsTemplate,
+    PostbackAction,
+    PushMessageRequest
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    PostbackEvent,
+    TextMessageContent
+)
+import os
+
+app = Flask(__name__)
+
+#CHANNEL_ACCESS_TOKEN and CHANNEL_SECRET
+configuration = Configuration(access_token=os.getenv('CHANNEL_ACCESS_TOKEN'))
+line_handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        line_handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+#remider PUSH
+@app.route("/remind", methods=["GET"])
+def remind():
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.push_message(
+            PushMessageRequest(
+                to="你的群組ID或userID",
+                messages=[TextMessage(text="💰 匯款提醒：今天是 1 號，記得匯款！")]
+            )
+        )
+    return "OK"
+
+# #加入好友
+# @handler.add(FollowEvent)
+# def handle_follow(event):
+#     print(f'Got {event.type} event')
+
+#postback
+@line_handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        if event.message.text == 'postback':
+            buttons_template = ButtonsTemplate(
+                title='Postback Sample',
+                text='Postback Action',
+                actions=[
+                    PostbackAction(label='Postback Action', text='Post Action Button CLicked!', data='postback'),#讓使用者發訊息
+                ])
+            template_message = TemplateMessage(
+                alt_text = 'Postback Sample',
+                template=buttons_template
+            )
+            line_bot_api.reply_message(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[template_message]
+                )
+            )
+@line_handler.add(PostbackEvent)
+def handle_postback(event):
+    if event.postback.data == 'postback':
+        print('Post Action Button Clickes!')
+
+#echo訊息
+# @handler.add(MessageEvent, message=TextMessageContent)
+# def handle_message(event):
+#     with ApiClient(configuration) as api_client:
+#         line_bot_api = MessagingApi(api_client)
+#         line_bot_api.reply_message_with_http_info(
+#             ReplyMessageRequest(
+#                 reply_token=event.reply_token,
+#                 messages=[TextMessage(text=event.message.text)]
+#             )
+#         )
+
+if __name__ == "__main__":
+    app.run()
